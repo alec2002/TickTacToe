1113cef07f189486ea117b13d7932f9245a3dd45
Alec <alec2002>
2018-01-03 19:31:04 -0800

    worked on tic tac toe
    worked on tic tac toe

--- src/TickTacToeLogic.java
+++ src/TickTacToeLogic.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
@@ -30,12 +32,24 @@ public int getPlayerTurn(){
 
 public boolean checkWin(int buttonLoc){
 	//iterate through states looking for winner
+	for (int i = 0; i < states.length; i++) {
+	if (states.equals(buttonLoc)) {
+		playerTurn = XTurn;
+		return true;
+	}
+	}
+	
+	playerTurn = OTurn;
+	return false;
 	//change player turn if false
+	
 }
 
 public void handleWinCondition(){
 	//Alert user of winner
+	JOptionPane.showMessageDialog(null, playerTurn + " won the game");
 	//ask for replay
+	
 }
 public void handleDraw(){
 	//Alert user of draw
